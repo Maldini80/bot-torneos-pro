534dbe5bbe03fa4f352209de003b0363f2942dfb
Maldini80 <dbzmaldini@gmail.com>
2025-07-17 17:26:47 +0200

    database.js

--- database.js
+++ database.js
@@ -8,19 +8,21 @@ if (!dbUrl) {
 }
 const client = new MongoClient(dbUrl);
 let db;
+
 export async function connectDb() {
     try {
         await client.connect();
-        db = client.db('tournamentBotDb');
+        db = client.db('tournamentBotDb'); // Se conecta a la base de datos correcta
         console.log('[DATABASE] Conectado exitosamente a MongoDB Atlas.');
     } catch (err) {
         console.error('[DATABASE] ERROR FATAL AL CONECTAR CON MONGODB:', err);
         process.exit(1);
     }
 }
+
 export function getDb() {
     if (!db) {
         throw new Error('La base de datos no ha sido conectada todavía.');
     }
-    return db;
+    return db; // Simplemente devuelve la conexión
 }
