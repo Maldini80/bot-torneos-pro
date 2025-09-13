945ebde67b79f6f6d2000e240280be6768833bd6
Maldini80 <dbzmaldini@gmail.com>
2025-09-13 16:48:44 +0200

    Import Team model in selectMenuHandler.js

--- src/handlers/selectMenuHandler.js
+++ src/handlers/selectMenuHandler.js
@@ -2,6 +2,7 @@
 
 import { getDb } from '../../database.js';
 import mongoose from 'mongoose';
+import Team from '../../src/models/team.js';
 import { TOURNAMENT_FORMATS, DRAFT_POSITIONS } from '../../config.js';
 import { ActionRowBuilder, ModalBuilder, StringSelectMenuBuilder, TextInputBuilder, TextInputStyle, EmbedBuilder, ButtonBuilder, ButtonStyle, UserSelectMenuBuilder, MessageFlags, PermissionsBitField } from 'discord.js';
 import { updateTournamentConfig, addCoCaptain, createNewDraft, handlePlayerSelection, createTournamentFromDraft, kickPlayerFromDraft, inviteReplacementPlayer, approveTeam, updateDraftMainInterface, updatePublicMessages, notifyVisualizer } from '../logic/tournamentLogic.js';
